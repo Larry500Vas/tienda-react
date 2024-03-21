975c0a1aa451623df3eb5d70f7467ceb9a671e7c
Larry fuentes <larryfuentes061@gmail.com>
2024-03-21 09:31:28 -0600

    primer commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,26 @@
+import './Products.css';
+
+export function Products(){
+    return(
+        <main className="products">
+            <h1>Saludos Des Products</h1>
+            <ul>
+                <li><img src="" alt="PRODUCTO" /></li>
+                <div>
+                    titulo - precio
+                </div>
+                <div>
+                    <button>ADD</button>
+                </div>
+                <li><img src="" alt="PRODUCTO2" /></li>
+                <div>
+                    titulo - precio
+                </div>
+                <div>
+                    <button>ADD</button>
+                </div>
+                
+            </ul>
+        </main>
+    )
+}
